6804c09d73b071a2067de707ad523574fbc84d2a
chenhongguang01 <chenhongguang01@baidu.com>
2018-10-24 22:06:46 +0800

    add

--- src/mini_nn.cc
+++ src/mini_nn.cc
@@ -6,7 +6,7 @@
 #include <algorithm>
 
 int main() {
-    NN::MINI_NN * mini_nn = new NN::MINI_NN("../data/train_data.tst", 30, 0.05);
+    NN::MINI_NN * mini_nn = new NN::MINI_NN("../data/train_data.tst", 30, 0.01);
     mini_nn->add_input_layer(784);
     mini_nn->add_layer(1000, "sigmoid");
     mini_nn->add_layer(800, "sigmoid");
@@ -16,6 +16,9 @@ int main() {
     mini_nn->fit();
 
     mini_nn->predict("../data/test_data.tst");
+
+    delete mini_nn;
+    mini_nn = NULL;
 
     return 0;
 }
